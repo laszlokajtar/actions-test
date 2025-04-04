b760033fc8140de448feff5ee04d1b19638fcfcf
lkajtar <lkajtar@gmail.com>
2025-04-04 22:40:11 +0200

    ruff

--- main.py
+++ main.py
@@ -4,14 +4,21 @@ import os
 
 load_dotenv()
 
+
 def main():
     print(f"This is a test: {os.getenv('IS_TEST')}")
-    
-    response = httpx.get("https://httpbin.org/bearer", headers={"Authorization": f'Bearer {os.getenv("API_KEY")}'})
+
+    response = httpx.get(
+        "https://httpbin.org/bearer",
+        headers={"Authorization": f"Bearer {os.getenv('API_KEY')}"},
+    )
 
     print(os.getenv("API_KEY"))
 
+    {   }
+
     print(response)
+
 
 if __name__ == "__main__":
     main()
